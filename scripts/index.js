2c1b81b8bc3510507cdec1e3e89935b0993ee544
Stif351 <tigerstif45@gmail.com>
2025-06-25 16:38:29 +0300

    Add scripts

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,56 @@
+let authors = document.querySelector('.title')
+let quotes = document.querySelector('.quote-text')
+let button = document.querySelector('.random')
+let share = document.querySelector('.share')
 
+let q = []
 
 fetch(
     "https://raw.githubusercontent.com/devchallenges-io/curriculum/refs/heads/main/3-javascript/challenges/group_1/data/random-quotes.json"
 )
     .then((response) => response.json())
     .then((data) => {
-        // Use the data here
+        q = data;
+        retukl(q)
     })
+
     .catch((error) => {
-        // Handle the error here
+
+        console.log(error);
     });
+
+
+const retukl = (q) => {
+
+    const ret = Math.floor(Math.random() * (q.length - 1))
+    const result = q[ret]
+    authors.innerHTML = result.author
+    quotes.innerHTML = result.quote
+
+}
+
+button.addEventListener('click', () => {retukl(q)})
+
+share.addEventListener('click', () => {
+
+
+    const quoteText = quotes.innerText;
+
+    const tempInput = document.createElement('textarea');
+    tempInput.value = quoteText;
+    document.body.appendChild(tempInput);
+
+    tempInput.select();
+    document.execCommand('copy');
+
+    document.body.removeChild(tempInput);
+
+    alert('Цитата скопійована в буфер обміну!');
+
+})
+
+
+
+
+
+
